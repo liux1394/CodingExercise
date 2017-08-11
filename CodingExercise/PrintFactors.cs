902ad416ed4ecbc87278908227fd9fa5ecd97436
Yin Liu <liux1394@umn.edu>
2017-08-10 22:45:20 -0700

    first commit

---
+++ CodingExercise/PrintFactors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercise
+{
+    class PrintFactors
+    {
+        private List<int> Solution()
+        {
+            List<int> res = null;
+            return res;
+        }
+
+        internal void Test()
+        {
+        }
+
+    }
+}
---
+++ CodingExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFactors inst = new PrintFactors();
+            inst.Test();
+            Console.ReadKey();
+        }
+    }
+}
